81be5c96bb488eb680f9cf7cda804a10df6fa6a4
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-08-16 14:05:44 -0700

    Added the renderQuestions functions and adjusted the myQuestions object

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,71 +3,54 @@ var startButton = document.querySelector("#start");
 var myQuestions = [
     {
         question: "Commonly used data types do not include:",
-        answers: {
-            a: 'boolean',
-            b: 'number',
-            c: 'string',
-            d: 'event listener'
-        },
-        correctAnswer: 'd'
+        answers: ['boolean', 'number', 'string', 'event listener'],
+        correctAnswer: 'event listener'
     },
     {
         question: "Which of the following is not a type of javaSript loop?",
-        answers: {
-            a: 'for',
-            b: 'do while',
-            c: 'if statements',
-            d: 'none of the above'
-
-        },
-        correctAnswer: 'c'
+        answers: ['for', 'do while', 'if statememts', 'none of the above'],
+        correctAnswer: 'if statements'
     },
     {
         question: "This is a valid javaScript string:\n 'I haven't seen it' ",
-        answers: {
-            a: 'true',
-            b: 'false',
-        },
-        correctAnswer: 'b'
+        answers: ['true', 'false'],
+        correctAnswer: 'false'
     },
     {
         question: "Ethical ways to get help on projects do NOT include:",
-        answers: {
-            a: 'asking someone more experienced',
-            b: 'googling',
-            c: 'chatGPT',
-            d: 'Using code that does not belong to you and not giving the author credit'
-        },
-        correctAnswer: 'd'
+        answers: ['asking someone more experienced', 'googling', 'chatGPT', 'Using code that does not belong to you and not giving the author credit'],
+        correctAnswer: 'Using code that does not belong to you and not giving the author credit'
     },
     {
         question: "Which is a commonly used coding language for web development?",
-        answers: {
-            a: 'javaScript',
-            b: 'Chrome',
-            c: 'Safari',
-            d: 'All of the above'
-        },
-        correctAnswer: 'a'
+        answers: ['javaScript', 'Chrome','Safari','All of the above'],
+        correctAnswer: 'javaScript'
     }
 ];
 
 var quizSpace = document.querySelector('#question');
 var answerSpace = document.querySelector('#answers');
 var loop = true;
+var questionNum = 0;
 
-function displayPossibleAnswers(){
-    for(var i = 0; i < 4; i++){
-        answerSpace.innerHTML.append()
-    }
-}
-
-startButton.addEventListener("click", function(){
-    var questionNum = 0;
+function renderQuestions(){
     do{
         quizSpace.innerHTML = myQuestions[questionNum].question;
-        answerSpace.innerHTML = myQuestions[questionNum].answers.a;
+        // This for loop will render the possible answers
+        for(var i = 0; i < myQuestions[questionNum].answers.length; i++){
+            let button = document.createElement('button')
+            button.innerText = myQuestions[questionNum].answers[i];
+            if(myQuestions[questionNum].answers[i] == myQuestions[questionNum].correctAnswer){
+                button.classList.add('correct');
+            }else{
+                button.classList.add('possible-answers');
+            }
+            answerSpace.appendChild(button);
+        }
         loop = false;
+
     }
     while(loop == true);
-});
+}
+// I'll have to make another function called startGame that the event listener will call that will include code for the timer and deducting time.
+startButton.addEventListener("click", renderQuestions)
